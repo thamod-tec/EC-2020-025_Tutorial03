5973bd14817f1aac658ae8fb6ef69c11dca6fcb2
thamod-tec <thamodrasmitha2015@gmail.com>
2024-11-17 21:32:42 +0530

    first commit

---
+++ src/main/java/com/example/practicalGuide_02/Service/StudentService.java
@@ -0,0 +1,16 @@
+package com.example.practicalGuide_02.Service;
+
+import java.util.List;
+
+import com.example.practicalGuide_02.Model.Student;
+
+public interface StudentService {
+    Student addStudent(Student student);
+    List<Student> getAllStudents();
+    Student getStudentById(long id);
+    Student updateStudent(Student student, long id);
+    void deleteStudent(long id);
+    List<Student> getStudentByYearsOfExperience(int yearsOfEnrollment);
+    String findDepartmentById(long id);
+    String deleteStudentByYearsOfEnrollment(int yearsOfEnrollment);
+}
